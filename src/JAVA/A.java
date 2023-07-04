35e6cc26b9377ba6e54f30d58be2a8af112e0f29
kimaruy <changmin2458@gmail.com>
2023-07-04 11:16:28 +0900

    세팅

---
+++ src/JAVA/A.java
@@ -0,0 +1,16 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		사람 a사람 = new 사람;
+		
+	}
+
+}
+class 사람 {
+	
+}
